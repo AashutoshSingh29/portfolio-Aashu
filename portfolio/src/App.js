62eb2b600bd0621bc3447835818aab5fc178c066
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-04 23:38:58 +0530

    Navbar Added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-purple-700">Hello world</h1>
+      {/* <h1 className="bg-purple-700">Hello world</h1> */}
+      <Navbar />
     </div>
   );
 }
