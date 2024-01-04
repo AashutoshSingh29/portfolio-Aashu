2c02dc3ebabea9ace8dbb0dd306fc3b4b367eb21
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-04 23:09:56 +0530

    React App created

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-purple-700">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
