3794c52fa4df44a5b8ff050beaca784cea0fbe99
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-06 23:50:38 +0530

    tailwind css removed from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,18 @@ import { IoReorderThree } from "react-icons/io5";
 const Header = () => {
   return (
     <>
-      <header className=" flex flex-row justify-between mx-4 bg-transparent">
-        <span className=" text-white font-serif font-bold my-4 ">
-          Aashutosh
-        </span>
-        <nav className="  w-1/3 my-1 hidden md:flex space-x-4  items-center justify-center border border-black-500 border-solid rounded-full bg-gray-500 hover:bg-slate-900 hover:text-white ">
-          <ul className="flex mx-3 mr-3 justify-between">
-            <li className="px-4 my-2 font-serif">Home</li>
-            <li className="px-4 my-2 font-serif">About</li>
-            <li className="px-4 my-2 font-serif">Qualification</li>
-            <li className="px-4 my-2 font-serif">Contact</li>
+      <header className="header-navbar">
+        <span className="header-logo">Aashutosh</span>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Qualification</li>
+            <li>Contact</li>
           </ul>
         </nav>
-        <div className="toogle text-white">
-          <IoReorderThree className=" size-11 md:hidden pr-2 " />
+        <div className="header-toogle">
+          <IoReorderThree className=" toogle" />
         </div>
       </header>
     </>
